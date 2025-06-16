6e545d788af66582b9d4783f6d26930056a0d9b9
Nikhil76785 <valivetinikhil2@gmail.com>
2025-06-16 19:47:59 +0530

    AVERAGE

---
+++ average.py
@@ -0,0 +1,12 @@
+tree1 = 68
+tree2 = 87
+tree3 = 77
+tree4 = 75
+tree5 = 90
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("The sum of the values is = ", sum)
+
+avg = sum/5
+
+print("The average of the trees is ", avg)
